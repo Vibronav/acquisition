7b3d6a1514203f76513c0a62717804aee4a10630
Adam Ćwikła <adam6xaolaw@gmail.com>
2024-03-27 19:51:09 +0100

    inital react app

---
+++ front_app/src/App.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Typography>hello world!</Typography>
+    </Fragment>
+  );
+}
+
+export default App;
