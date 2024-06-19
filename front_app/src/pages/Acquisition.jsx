4e876975c275bc6d33f3759b0703d5f3aed162a3
Karolina Zrobek <karolina.m.zrobek@gmail.com>
2024-06-19 20:37:50 +0200

    Add audio player

--- front_app/src/pages/Acquisition.jsx
+++ front_app/src/pages/Acquisition.jsx
@@ -1,4 +1,4 @@
-import { Container, FormControl, InputLabel, MenuItem, Select, Stack, Typography } from '@mui/material';
+import {  FormControl, InputLabel, MenuItem, Select, Stack, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
