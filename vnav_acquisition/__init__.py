7c5ed5995c64f9a949292eb72c50ff382ec7ee7c
Gawron97 <jakubgawron97@gmail.com>
2025-06-16 17:13:28 +0200

    download model only when no exists

--- vnav_acquisition/__init__.py
+++ vnav_acquisition/__init__.py
@@ -1,14 +1,16 @@
 import gdown
 import os
 
-gdown.download(
-    "https://drive.google.com/uc?id=1dRyy8ar81iSZshwz9yWInNtckmtXDhud",
-    "goturn.caffemodel",
-    quiet=False
-)
+if not os.path.exists("goturn.caffemodel"):
+    gdown.download(
+        "https://drive.google.com/uc?id=1dRyy8ar81iSZshwz9yWInNtckmtXDhud",
+        "goturn.caffemodel",
+        quiet=False
+    )
 
-gdown.download(
-    "https://drive.google.com/uc?id=1uwy0Rp8wFfRA7YTeOnMnkdThdaEcqOO7",
-    "goturn.prototxt",
-    quiet=False
-)
+if not os.path.exists("goturn.prototxt"):
+    gdown.download(
+        "https://drive.google.com/uc?id=1uwy0Rp8wFfRA7YTeOnMnkdThdaEcqOO7",
+        "goturn.prototxt",
+        quiet=False
+    )
