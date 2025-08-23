73000fb2df88d4751b642bbc7292dcebf40ac074
Gawron97 <jakubgawron97@gmail.com>
2025-08-23 18:37:45 +0200

    refactor sync code add posibility to sync needle tracking files

--- vnav_acquisition/sync.py
+++ vnav_acquisition/sync.py
@@ -11,6 +11,7 @@ import glob
 from collections import defaultdict
 from scipy.signal import stft, correlate2d, firwin, filtfilt, fftconvolve, windows
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def debug_plot_sync(ref, input_spec, idx_sync):
     plt.figure(figsize=(12, 6))
@@ -107,23 +108,16 @@ def find_delay_by_sync(video_file, audio_file, audio_channel, debug_plots, video
         return audio_delay, audio_fs
 
 
-def add_audio_annotations(video_file, audio_file, annotation_file, audio_channel, debug_plots):
+def modify_annotation_file(annotation_file, audio_file, audio_delay, audio_fs):
     with open(annotation_file) as f:
         annotation_set = json.load(f)
+
     if "audio_annotations" in annotation_set:
-        return f"'audio_annotations' already present in : {annotation_file}"
-
-    audio_delay, audio_fs = find_delay_by_sync(video_file, audio_file, audio_channel, debug_plots)
-
-    if audio_delay is None:
-        return f"Video file sound corrupted: {video_file}"
-    if audio_fs is None:
-        return f"Audio file sound corrupted: {audio_file}"
-
-    audio_annotations = list()
+        return
+    
     video_annotations = annotation_set["video_annotations"]
     if type(video_annotations) is list:
-        # interval annotations
+        audio_annotations = list()
         for video_annotation in video_annotations:
             audio_annotation = dict()
             audio_annotation["start_time"] = video_annotation["start_time"] + audio_delay
@@ -132,19 +126,40 @@ def add_audio_annotations(video_file, audio_file, annotation_file, audio_channel
             audio_annotation["end_sample"] = int(audio_annotation["end_time"] * audio_fs)
             audio_annotations.append(audio_annotation)
     elif type(video_annotations) is dict:
-        # event annotations
         audio_annotations = defaultdict(dict)
         for event, video_annotation in video_annotations.items():
             audio_annotations[event]["time"] = video_annotation["time"] + audio_delay
             audio_annotations[event]["sample"] = int(audio_annotations[event]["time"] * audio_fs)
     else:
-        raise Exception("Unknown annotation format")
+        print(f'Annotation file has unknown format! : {annotation_file}')
 
     annotation_set["audio_file"] = os.path.basename(audio_file)
     annotation_set["audio_annotations"] = audio_annotations
 
     with open(annotation_file, 'w') as f:
         json.dump(annotation_set, f, indent=4)
+
+def modify_needle_position_file(needle_position_file, audio_delay):
+    df = pd.read_csv(needle_position_file)
+    df["Audio time (s)"] = df["Time (s)"] + audio_delay
+    df.to_csv(needle_position_file, index=False)
+
+def add_audio_annotations(video_file, audio_file, annotation_file, needle_position_file, audio_channel, debug_plots):
+
+    audio_delay, audio_fs = find_delay_by_sync(video_file, audio_file, audio_channel, debug_plots)
+
+    print(audio_delay)
+
+    if audio_delay is None:
+        return f"Video file sound corrupted: {video_file}"
+    if audio_fs is None:
+        return f"Audio file sound corrupted: {audio_file}"
+
+    if annotation_file is not None:
+        modify_annotation_file(annotation_file, audio_file, audio_delay, audio_fs)
+
+    if needle_position_file is not None:
+        modify_needle_position_file(needle_position_file, audio_delay)
 
 
 def main():
@@ -158,6 +173,8 @@ def main():
                         help="Path to video (webm, mp4) files (default: same as audio files)")
     parser.add_argument("--annotation-path", type=str, default="", required=False,
                         help="Path to annotations files (default: same as audio files)")
+    parser.add_argument("--needle-position-path", type=str, help="Path to needle position files" \
+                         " If not provided needle position files will not be processed")
     parser.add_argument("--audio-channel", type=int, default=-1,
                         help="Index of channel in WAV audio file to use for sync (e.g. 0=left, 1=right, -1=last). Default: -1")
     parser.add_argument("--debug-plots", action="store_true", help="Show debug plots for synchronization")
@@ -167,38 +184,62 @@ def main():
         args.video_path = args.audio_path
     if not args.annotation_path:
         args.annotation_path = args.audio_path
-
-    print(f"Audio path: {args.audio_path}")
-
-    audio_files = {os.path.splitext(os.path.basename(f))[0].replace(args.audio_suffix, ""): f
-                   for f in glob.glob(args.audio_path + os.sep + "*.wav")}
-    video_files = {os.path.splitext(os.path.basename(f))[0]: f
-                   for f in glob.glob(args.video_path + os.sep + "*.*") if os.path.splitext(f)[1] in (".webm", ".mp4")}
-    annotation_files = {os.path.splitext(os.path.basename(f))[0]: f
-                        for f in glob.glob(args.annotation_path + os.sep + "*.json")}
     
-    print(audio_files)
-    print(annotation_files)
 
+    audio_files = {
+        os.path.splitext(os.path.basename(f))[0].replace(args.audio_suffix, ""): os.path.join(args.audio_path, f)
+        for f in os.listdir(args.audio_path)
+        if os.path.isfile(os.path.join(args.audio_path, f)) and f.endswith('.wav')
+    }
+    
+    video_files = {
+        os.path.splitext(os.path.basename(f))[0]: os.path.join(args.video_path, f)
+        for f in os.listdir(args.video_path)
+        if os.path.isfile(os.path.join(args.video_path, f)) and f.endswith(('.webm', '.mp4'))
+    }
+    
+    annotation_files = {
+        os.path.splitext(os.path.basename(f))[0]: os.path.join(args.annotation_path, f)
+        for f in os.listdir(args.annotation_path)
+        if os.path.isfile(os.path.join(args.annotation_path, f)) and f.endswith('.json')
+    }
+    
+    needle_position_files = {}
+    if args.needle_position_path:
+        needle_position_files = {
+            os.path.splitext(os.path.basename(f))[0]: os.path.join(args.needle_position_path, f)
+            for f in os.listdir(args.needle_position_path)
+            if os.path.isfile(os.path.join(args.needle_position_path, f)) and f.endswith('.csv')
+        }
+    
     debug_plots = args.debug_plots
     audio_channel = args.audio_channel
+
+    files_to_process = set(annotation_files.keys() | needle_position_files.keys())
     skipped = []
-    for name, annotation_file in annotation_files.items():
-        audio_present = name in audio_files
-        video_present = name in video_files
+    for filename in files_to_process:
+        audio_present = filename in audio_files
+        video_present = filename in video_files
         if audio_present and video_present:
-            print(f"Processing {name}")
-            result = add_audio_annotations(video_files[name], audio_files[name], annotation_file, audio_channel, debug_plots)
+            print(f"Processing {filename}")
+            result = add_audio_annotations(
+                video_files[filename], 
+                audio_files[filename], 
+                annotation_files[filename] if filename in annotation_files else None,
+                needle_position_files[filename] if filename in needle_position_files else None,
+                audio_channel, 
+                debug_plots
+            )
             if result:
                 print(result)
                 skipped.append(result)
         else:
             if not audio_present:
-                result = f"Missing audio for: {name}"
+                result = f"Missing audio for: {filename}"
                 print(result)
                 skipped.append(result)
             if not video_present:
-                result = f"Missing video for: {name}"
+                result = f"Missing video for: {filename}"
                 print(result)
                 skipped.append(result)
     if skipped:
