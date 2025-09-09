93036e8fe71d0a733f95fbe0a6cd3388b112b9d4
Gawron97 <jakubgawron97@gmail.com>
2025-09-09 11:57:02 +0200

    uncomment raspberrypi code

--- vnav_acquisition/record.py
+++ vnav_acquisition/record.py
@@ -13,8 +13,8 @@ def start_recording(output_filename_prefix, socketio_instance):
         "filename": video_filename
     })
 
-    # is_started = on_rec_start(config['connection'], socketio_instance, audio_filename)
-    is_started = True
+    is_started = on_rec_start(config['connection'], socketio_instance, audio_filename)
+    # is_started = True
     if not is_started:
         socketio_instance.emit("record", {
             "action": "stop",
@@ -25,8 +25,8 @@ def start_recording(output_filename_prefix, socketio_instance):
     return True
 
 def stop_recording(socketio_instance):
-    # is_recorded = on_rec_stop()
-    is_recorded = True
+    is_recorded = on_rec_stop()
+    # is_recorded = True
     if not is_recorded:
         socketio_instance.emit("record", {
             "action": "stop",
