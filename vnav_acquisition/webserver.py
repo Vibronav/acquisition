97a99f3263052281d534520adbac7eca3c6d2e6c
Gawron97 <jakubgawron97@gmail.com>
2025-10-01 16:26:17 +0200

    remove mock methods

--- vnav_acquisition/webserver.py
+++ vnav_acquisition/webserver.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import base64
 from flask import Flask, request, jsonify, send_from_directory
-from vnav_acquisition.comm import is_ssh_connected, ssh_connect, on_rec_start, on_rec_stop, mock_ssh_connect
+from vnav_acquisition.comm import is_ssh_connected, ssh_connect, on_rec_start, on_rec_stop
 from vnav_acquisition.config import config
 from vnav_acquisition.runtime_config import runtime_config
 from vnav_acquisition.automation import safe_run_automation
@@ -267,7 +267,7 @@ def main():
     url = "http://127.0.0.1:{0}".format(port)
     IP_FILE.write_text(get_local_ip_address())
 
-    mock_ssh_connect(socketio)
+    ssh_connect(*config['connection'], socketio_instance=socketio)
 
     threading.Timer(1.0, lambda: webbrowser.open(url)).start()
     
